75d506552952e3a4aacd26251e928e73bfee3ed8
zogstrip <regis@hanol.fr>
2025-12-17 19:23:50 +0100

    DEV: Use new addSaveableCustomFields plugin API
    Replace manual `modifyClass` controller override with the new
    `addSaveableCustomFields({ page: "notifications" })` API.
    
    The previous approach of pushing to `saveAttrNames` in a `save()` override
    was broken because `saveAttrNames` is now a getter returning a fresh array.
    
    cf. https://github.com/discourse/discourse/pull/36757

--- assets/javascripts/discourse/initializers/extend-for-solved-reminders.js
+++ assets/javascripts/discourse/initializers/extend-for-solved-reminders.js
@@ -1,16 +1,5 @@
-import { action } from "@ember/object";
 import { apiInitializer } from "discourse/lib/api";
 
 export default apiInitializer((api) => {
-  api.modifyClass(
-    "controller:preferences/notifications",
-    (Superclass) =>
-      class extends Superclass {
-        @action
-        save() {
-          this.saveAttrNames.push("custom_fields");
-          super.save(...arguments);
-        }
-      }
-  );
+  api.addSaveableCustomFields({ page: "notifications" });
 });
