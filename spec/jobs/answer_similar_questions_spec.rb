0f0d25108709aaed5f84662c22af018a7f42f3d9
David Battersby <info@davidbattersby.com>
2026-03-10 14:25:39 +0400

    Revert "DEV: use solved services in tests (#38)" (#39)
    This reverts commit e4500c244d955f2df8ff762ab89e4fb780979306.

--- spec/jobs/answer_similar_questions_spec.rb
+++ spec/jobs/answer_similar_questions_spec.rb
@@ -9,23 +9,13 @@ describe Jobs::AnswerSimilarQuestions do
 
   describe "#execute" do
     fab!(:user)
-    fab!(:topic) do
-      Fabricate(:topic_with_op, last_post_user_id: user.id, last_posted_at: 170.days.ago)
-    end
+    fab!(:topic) { Fabricate(:topic, last_post_user_id: user.id, last_posted_at: 170.days.ago) }
     fab!(:post) { Fabricate(:post, topic: topic, user: user) }
 
-    before do
-      5.times { Fabricate(:topic) }
-
-      DiscourseSolved::AcceptAnswer.call!(
-        params: {
-          post_id: post.id,
-        },
-        guardian: Discourse.system_user.guardian,
-      )
-    end
+    before { 5.times { Fabricate(:topic) } }
 
     it "should send a PM to user suggesting more topics to answer" do
+      DiscourseSolved.accept_answer!(post, Discourse.system_user)
       expect { described_class.new.execute({ post_id: post.id }) }.to change { Topic.count }.by(1)
       expect(Topic.last.title).to eq(I18n.t("answer_similar_questions.title"))
       expect(Topic.last.first_post.raw).to include(
